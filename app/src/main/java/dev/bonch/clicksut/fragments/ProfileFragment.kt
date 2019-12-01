46063a63838e8c70d6d5b2de06fd13e064e8f836
Sery0077 <52316851+Sery0077@users.noreply.github.com>
2019-12-01 15:28:12 +0300

    ver 2.1

--- app/src/main/java/dev/bonch/clicksut/fragments/ProfileFragment.kt
+++ app/src/main/java/dev/bonch/clicksut/fragments/ProfileFragment.kt
@@ -9,19 +9,29 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dev.bonch.clicksut.R
 import dev.bonch.clicksut.activities.SignInActivity
+import dev.bonch.clicksut.models.Post
+import dev.bonch.clicksut.ui.PostAdapter
 import kotlin.math.sign
 
-class ProfileFragment: Fragment() {
+class ProfileFragment : Fragment() {
 
     private lateinit var fullNameTw: TextView
     private lateinit var vkLinkTw: TextView
+    private lateinit var dataBase: FirebaseFirestore
 
     private lateinit var avatarIv: ImageView
     private lateinit var signOutBtn: Button
     private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var posts: ArrayList<Post>
 
 
     override fun onCreateView(
@@ -33,13 +43,45 @@ class ProfileFragment: Fragment() {
         mAuth = FirebaseAuth.getInstance()
         signOutBtn = view.findViewById(R.id.sign_out_button)
 
-        signOutBtn.setOnClickListener{
+        dataBase = FirebaseFirestore.getInstance()
+
+        getPosts()
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.boardRecyclerView)
+
+        posts = ArrayList<Post>()
+
+        recyclerView.adapter = PostAdapter(posts)
+        recyclerView.layoutManager =
+            LinearLayoutManager(BoardFragment@ context, LinearLayoutManager.VERTICAL, false)
+
+        signOutBtn.setOnClickListener {
             mAuth.signOut()
 
-            val intent = Intent(MainAppActivity@context, SignInActivity::class.java)
+            val intent = Intent(MainAppActivity@ context, SignInActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
+
         return view
+    }
+
+    private fun getPosts() {
+        dataBase.collection("lesnoe")
+            .get()
+            .addOnSuccessListener { it ->
+                it.forEach {
+                    if (it.get("userName").toString() == "Сергей") {
+                        posts.add(
+                            Post(
+                                it.get("description").toString(),
+                                it.get("userName").toString(),
+                                it.get("postImage").toString()
+                            )
+                        )
+                        recyclerView.adapter!!.notifyDataSetChanged()
+                    }
+                }
+            }
     }
 }
